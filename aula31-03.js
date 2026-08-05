266738abad0b9e57c752171fe78b17e7beb19b88
RenataOli45 <renataoliveiraribeiro231@gmail.com>
2026-08-05 20:45:13 -0300

    Add files via upload

---
+++ aula31-03.js
@@ -0,0 +1,25 @@
+let nomeAluno = "Renata";
+
+console.log(nomeAluno)
+
+let idade = 19;
+
+let saudacao = "Olá " + nomeAluno + "como está?"
+
+console.log(saudacao)
+
+if(idade >= 18) {
+    console.log("Você é maior de idade!")
+} else {
+    console.log("você é maior de idade.")
+}
+ //usa competição de Natação
+ //Se a idade for de menor que 12 anos = competição infantil
+ //Se a idade for entre 12 e 15 anos = competição infanto-juvenil
+ //Se a idade for 15 e 17 = competição juvenil
+ //Se a idade for maior de 18 anos = competição adulta
+ //Você insira a idade de competir e o nome e o programa imprima:
+ // "Fulano, você participará da competição xxxxx"
+
+ //if(idade >= 12 && idade <= 15 ) {
+ // console.log(nomeCompetidor + ", você participará da competição infanto juvenil")
