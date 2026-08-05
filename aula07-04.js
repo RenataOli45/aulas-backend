f121112b8599c67173c5b096be498f26e3215641
RenataOli45 <renataoliveiraribeiro231@gmail.com>
2026-08-05 20:46:10 -0300

    Add files via upload

---
+++ aula07-04.js
@@ -0,0 +1,23 @@
+let idade; //DECLARANDO A VARIÁVEL
+idade = 11; //INSTANCIANDO A VARIAVEL (COLOCANDO VALOR NELA)
+//let idade = 17; -> fazer as duas coisas na mesma linhas
+
+let preco;
+
+if(idade < 12) {
+    preco = 15.00
+} else if(idade >= 12 && idade <= 17) {
+    preco = 20.00
+} else {
+    preco = 40.00
+}
+
+console.log("você tem " + idade + " anos. Seu ingresso custa: R$" + preco)
+
+
+idade = 20;
+idade == 17;
+idade > 17;
+idade < 17;
+idade <= 17;
+idade >= 17;
